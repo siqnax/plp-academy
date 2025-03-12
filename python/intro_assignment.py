bf05bb71ec0c25ca3073efbf457bbbbf406f160f
siqnax <32621415+siqnax@users.noreply.github.com>
2025-03-13 01:49:17 +0300

    Update intro_assignment.py

--- python/intro_assignment.py
+++ python/intro_assignment.py
@@ -1,1 +1,25 @@
-week 1 assignment
+print ("Python Intro assignment")
+
+Num1 = float(input("Enter first number: "))
+Num2 = float(input("Enter second number: "))
+operator = input("Enter the operator (+,-, *, /): ")
+
+if operator == "+": 
+    print(Num1, "+" , Num2, "=", Num1 + Num2) 
+
+elif operator == "-": 
+    print(Num1, "-" , Num2, "=", Num1 - Num2) 
+
+elif operator == "*": 
+    print(Num1, "*" , Num2, "=", Num1 * Num2) 
+
+elif operator == "/": 
+    print(Num1, "/" , Num2, "=", Num1 / Num2) 
+
+else:
+    print("Invalid input")
+
+
+
+
+
